93274faef34a2eb4466ea549a8f7e6c807d8169d
BruceZhang54110 <zhw54110@163.com>
2024-08-31 11:35:56 +0800

    重构以改进模块化与错误处理

--- crates/minigrep/src/lib.rs
+++ crates/minigrep/src/lib.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{env, fs};
 use std::error::Error;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 // 创建构造函数
@@ -18,9 +19,34 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config {query, file_path, })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {query, file_path, ignore_case})
     }
 
+}
+
+/// 使用‘a 生命周期标注，保证参数 content 和 返回数组的生命周期是相同的
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+/// 使用‘a 生命周期标注，保证参数 content 和 返回数组的生命周期是相同的
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 /// 错误类型，使用了 trait 对象 Box<dyn Error>
@@ -31,6 +57,49 @@ impl Config {
 /// Result 之后的 ? 是传播错误的简写
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", contents);
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    if results.is_empty() {
+        println!("未找到字符串:{}", &config.query);
+    } else {
+        for line in results {
+            println!("{line}");
+        }
+    }
+
     Ok(())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "safe";
+        let contents = "\
+Rsut:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_result() {
+        let query = "Safe";
+        let contents = "\
+Rsut:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search_case_insensitive(query, contents));
+    }
+
+}
+
+
+
--- crates/minigrep/src/main.rs
+++ crates/minigrep/src/main.rs
@@ -4,14 +4,14 @@ use minigrep::Config;
 
 
 fn main() {
-    println!("Hello, world!, This is minigrep");
+    println!("Hello, world!, This is minigrep, search your words");
     // 获取命令行参数
     let args: Vec<String> = env::args().collect();
     // 参数赋值
     // 如果没发生panic!,unwrap 会返回 Ok内部封装的值，
     // 当值是Err是，该方法会调用一个闭包
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         // 立即停止程序并将传递给它的数字作为退出状态码
         process::exit(1);
     });
@@ -24,7 +24,7 @@ fn main() {
 
     // 使用 if let 检查run 是否返回一个Err,只关心检测错误
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
     
