1abcc59bb71800418a3987b0b8b0754238b8544a
BruceZhang54110 <zhw54110@163.com>
2024-10-20 21:16:03 +0800

    match Result

--- crates/error_demo/src/main.rs
+++ crates/error_demo/src/main.rs
@@ -1,5 +1,20 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    // panic!("this is a panic!");
-    let v = vec![1, 2, 3];
-    v[99];
+    // panic!("This is a panic");
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file {:?}", e),
+            },
+            other_error => panic!("Error opening the file: {:?}", other_error),
+        },
+    };
 }
