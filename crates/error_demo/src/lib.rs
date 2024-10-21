5a5a5ad6e45093a6d21a8b9e71fab85b1f36dbb5
BruceZhang54110 <zhw54110@163.com>
2024-10-21 14:15:13 +0800

    ? 运算符传递错误

--- crates/error_demo/src/lib.rs
+++ crates/error_demo/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, io::{Error, Read}};
 
 fn open_file_unwrap() {
     let f = File::open("hello.txt").unwrap();
@@ -6,6 +6,34 @@ fn open_file_unwrap() {
 
 fn open_file_expect() {
     let f = File::open("hello.txt").expect("无法打开文件");
+}
+
+fn read_username_from_file() -> Result<String, Error> {
+    let username_file_result = File::open("hello.txt");
+    let mut user_name_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match user_name_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_two() -> Result<String, Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_three() -> Result<String, Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
 
 #[cfg(test)]
@@ -20,5 +48,13 @@ mod tests {
     #[test]
     fn test_open_file_expect() {
         open_file_expect();
+    }
+
+    // 测试错误传递
+    #[test]
+    fn test_read_username_from_file() {
+        // assert_eq!("my name", read_username_from_file().unwrap());
+        // assert_eq!("my name", read_username_from_file_two().unwrap());
+        assert_eq!("my name", read_username_from_file_three().unwrap());
     }
 }
