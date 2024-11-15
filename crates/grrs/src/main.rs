a9e8dbe059f6a070cd8701fa6904b12bf4cd4942
BruceZhang54110 <zhw54110@163.com>
2024-11-15 10:51:05 +0800

    进度条测试

--- crates/grrs/src/main.rs
+++ crates/grrs/src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use anyhow::{Context, Ok, Result};
 use log::{info, warn};
+use std::fmt::Write;
+use indicatif::{ProgressBar, ProgressStyle, ProgressState};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -19,11 +21,28 @@ fn main() -> Result<()> {
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    let line_count = content.lines().count();
+    // 进度条
+    let bar: ProgressBar = ProgressBar::new(line_count as u64);
+    bar.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
+
+    let mut result = String::new();
 
     for line in content.lines() {
+        // 进度条递增
+        bar.inc(1);
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            //println!("{}", line);
+            result.push_str(line);
+            result.push_str("\n");
         }
     }
+    // 进度结束
+    bar.finish();
+
+    println!("search result: {}", result);
     Ok(())
 }
