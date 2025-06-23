a0bffe05943d8ad6825ddc68d717974d812e1c28
BruceZhang54110 <zhw54110@163.com>
2025-06-23 08:52:29 +0800

    future channel async

--- crates/hello-async/examples/channel1.rs
+++ crates/hello-async/examples/channel1.rs
@@ -4,8 +4,29 @@ fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
+        let tx1 = tx.clone();
+
         // 发送放到一个future 中
-        let tx_future = async {
+        let tx1_future = async move {
+            let vals = vec![
+                String::from("Hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_future = async {
+            while let Some(value) = rx.recv().await {
+                println!("received: {value}");
+            }
+        };
+
+        let tx_future = async move {
             let vals = vec![
                 String::from("Hi"),
                 String::from("from"),
@@ -17,14 +38,8 @@ fn main() {
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
-
-        let rx_future = async {
-            while let Some(value) = rx.recv().await {
-                println!("received: {value}");
-            }
-        };
         // 使用 join 接收两个future，返回一个新的future
-        trpl::join(tx_future, rx_future).await;
+        trpl::join3(tx1_future, tx_future, rx_future).await;
     });
 
 
